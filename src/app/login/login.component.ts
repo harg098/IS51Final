f669d39664f53f90872138b0a797724eb573fb7e
Harinder Gill <harinder112@hotmail.com>
2018-12-20 19:46:00 -0800

    Pre commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,19 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from '../localStorageService';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
 import { Subject } from 'rxjs';
+
+export interface IUser {
+  id?: number;
+  username: string;
+  password: string;
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user: IUser = {
+    username: null,
+    password: null
+  };
 
   constructor(private router: Router, private toastService: ToastService) {
   }
 
   ngOnInit() {
 
+  }
+
+  login(user: IUser) {
+    const presetUser = { username: 'harg098', password: 'test123' };
+    if (user.username != null && user.password != null &&
+      user.username !== '' && user.password !== '') {
+      if (user.username === presetUser.username &&
+        user.password === presetUser.password) {
+        localStorage.setItem('user', JSON.stringify(user));
+        this.router.navigate(['contacts', user]);
+      } else {
+        this.toastService.showToast('warning', 2000, 'Username or password is wrong!');
+      }
+    } else {
+      this.toastService.showToast('danger', 2000, 'Must specify credentials');
+    }
   }
 
 }
